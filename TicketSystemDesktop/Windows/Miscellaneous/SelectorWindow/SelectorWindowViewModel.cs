27c37f17709be3e9b8bac577c8db1fea260f814a
xrou <royashen@mail.ru>
2024-05-01 13:20:58 +0700

    filter in selector window

--- TicketSystemDesktop/Windows/Miscellaneous/SelectorWindow/SelectorWindowViewModel.cs
+++ TicketSystemDesktop/Windows/Miscellaneous/SelectorWindow/SelectorWindowViewModel.cs
@@ -14,13 +14,26 @@ namespace TicketSystemDesktop
     public class SelectorWindowViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<IDbEntity> Items { get; set; } = new ObservableCollection<IDbEntity>();
+        public ObservableCollection<IDbEntity> DisplayItems { get; set; } = new ObservableCollection<IDbEntity>();
         public IDbEntity SelectedEntity { get; set; }
+
+        private string filterText = "";
+        public string FilterText
+        {
+            get { return filterText; }
+            set
+            {
+                filterText = value;
+                DisplayItems = new ObservableCollection<IDbEntity>(Items.Where(x => (x.MainParameter.ToString()).ToLower().Contains(filterText.ToLower())));
+            }
+        }
 
         private Window window;
 
         public SelectorWindowViewModel(Window window, ObservableCollection<IDbEntity> items)
         {
             Items = items;
+            DisplayItems = Items;
             this.window = window;
         }
 
