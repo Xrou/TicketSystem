2f9c7123126ae5cd0ec9487059c9cc53f58058e3
xrou <royashen@mail.ru>
2024-06-24 17:32:03 +0700

    created logger class

---
+++ TicketSystemDesktop/Miscellaneous/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemDesktop.Miscellaneous
+{
+    public class Logger
+    {
+        public static void Log(LogStatus status, string caller, string message)
+        {
+            DateTime now = DateTime.Now;
+            File.AppendAllText("./Log/", $"{now} {status}\nAt {caller}\n\nMessage:\n{message}");
+        }
+    }
+
+    public enum LogStatus
+    {
+        Info,
+        Warning, 
+        Error
+    }
+}
